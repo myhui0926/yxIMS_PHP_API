03822d3b448e66d765f4953dfc0baa5ebdd2ba23
wubantu <myhui0926@outlook.com>
2017-10-09 08:53:44 +0800

    '站点通用代码,链接数据库,设置通用header'

---
+++ common/set_header_mysql.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hui
+ * Date: 2017/9/27
+ * Time: 19:05
+ */
+header('Access-Control-Allow-Origin:http://localhost:8080');
+//header('Content-Type:application/json;charset=UTF-8');
+header('Content-Type:application/json;charset=UTF-8');
+require '../mysqli_connect.php';
